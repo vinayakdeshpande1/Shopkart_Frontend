b3475d12c413b146224d67202708ba7c006a192e
= <=>
2021-10-15 00:47:11 +0530

    created carousel for homepage

---
+++ src/app/homepage-carousel/homepage-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-carousel',
+  templateUrl: './homepage-carousel.component.html',
+  styleUrls: ['./homepage-carousel.component.css']
+})
+export class HomepageCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
