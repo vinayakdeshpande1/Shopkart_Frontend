eb9f0be974f8a6be87887b4d21c7b080709809a4
= <=>
2021-10-25 14:02:29 +0530

    click on individual product to see it

--- src/app/view-all-page/view-all-page.component.ts
+++ src/app/view-all-page/view-all-page.component.ts
@@ -16,6 +16,6 @@ export class ViewAllPageComponent implements OnInit {
     alert("Product added to Cart")
   }
 
-  product = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  product = [1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 
 }
