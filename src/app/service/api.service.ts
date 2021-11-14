199a1932810e47a30597f4bcfc13c1f1f0d2c076
= <=>
2021-11-14 15:32:53 +0530

    share user to all components if logged in

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  user = new BehaviorSubject(localStorage.getItem("user") || "")
+  currentUser = this.user.asObservable()
+
   constructor() { }
+
+  updateUser(fullname:string) {
+    this.user.next(fullname)
+  }
+  
 }
