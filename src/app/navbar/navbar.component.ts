19ac71d9abdfd055e46c124c31e984f3f436565a
= <=>
2021-11-14 15:30:10 +0530

    login/logout logic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  user:string = ""
 
   ngOnInit(): void {
+    this.api.currentUser.subscribe(user => this.user = user)
+  }
+
+  logout() {
+    this.api.updateUser("")
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
   }
 
 }
