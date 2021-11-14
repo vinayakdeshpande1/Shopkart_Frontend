9ed003b880d0cc902a8aff5c94f4372ea68c7a5d
= <=>
2021-11-14 16:09:07 +0530

    logged in for dynamic form values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../service/api.service';
-// import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +12,34 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private api:ApiService) { }
 
+  email:string=""
+  password:string=""
+
+  public loginForm = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.minLength(4))
+  })
+
   ngOnInit(): void {
     if (localStorage.getItem("token")) {
       this.router.navigate(["/"])
     }
   }
 
-  async login() {
+  async login($event:any) {
+    // console.log(this.loginForm.value);
+    console.log(this.email, this.password);
+    
+    
+    
     await fetch("http://localhost:3300/login", {
       method: "post",
       headers: {
         'content-Type': "application/json"
       },
       body: JSON.stringify({
-        "email": "vin@vin.com",
-        "password": "1234",
+        email: this.email,
+        password: this.password,
       })
     })
       .then((respone) => respone.json())
