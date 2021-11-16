0e521b7c76f7855caeb8c3b0c6e0c16bc5250f3e
= <=>
2021-11-16 23:27:31 +0530

    made token available via auth-service

--- src/app/auth-service/api.service.ts
+++ src/app/auth-service/api.service.ts
@@ -8,21 +8,25 @@ export class ApiService {
 
   user = new BehaviorSubject(localStorage.getItem("user") || "")
   currentUser = this.user.asObservable()
-  isLoggedIn = false
+  isLoggedIn:Boolean = localStorage.getItem("token")!==null || false
   token = localStorage.getItem("token") || null
 
   constructor() { }
 
   updateUser(fullname: string) {
     this.user.next(fullname)
-    this.isLoggedIn = true
+    if (localStorage.getItem("token")) {
+      this.isLoggedIn = true
+    }
   }
 
   logout() {
     this.updateUser("")
-    this.isLoggedIn = false
     localStorage.removeItem("token")
     localStorage.removeItem("user")
+    if (!localStorage.getItem("token")) {
+      this.isLoggedIn = false
+    }
   }
 
 }
