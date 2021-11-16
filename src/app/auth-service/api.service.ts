fa1d5354c669d7a5424c4154cf910121e413c198
= <=>
2021-11-16 23:05:32 +0530

    added isLoggedIn variable

--- src/app/auth-service/api.service.ts
+++ src/app/auth-service/api.service.ts
@@ -8,17 +8,20 @@ export class ApiService {
 
   user = new BehaviorSubject(localStorage.getItem("user") || "")
   currentUser = this.user.asObservable()
+  isLoggedIn = false
 
   constructor() { }
 
-  updateUser(fullname:string) {
+  updateUser(fullname: string) {
     this.user.next(fullname)
+    this.isLoggedIn = true
   }
 
   logout() {
     this.updateUser("")
+    this.isLoggedIn = false
     localStorage.removeItem("token")
     localStorage.removeItem("user")
   }
-  
+
 }
