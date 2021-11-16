1fdc3c482cf134710016a211803d3be5c13abc84
= <=>
2021-11-16 23:06:04 +0530

    moved home/cart to app folder and deleted pages folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './pages/cart/cart.component';
-import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 import {SignupComponent} from './signup/signup.component';
 import {LoginComponent} from './login/login.component';
 import { ViewAllPageComponent } from './view-all-page/view-all-page.component';
