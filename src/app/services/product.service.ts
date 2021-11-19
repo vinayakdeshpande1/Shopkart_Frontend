c911319d7329da872f75df5baf7ae41752888e55
= <=>
2021-11-20 00:31:12 +0530

    created fetchProduct

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../auth-service/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private auth: ApiService) { }
+
+  async fetchProduct(productId: string) {
+    return await fetch(`http://localhost:3300/products/${productId}`)
+      .then((res) => res.json())
+      .then((data) => data.data)
+  }
 }
