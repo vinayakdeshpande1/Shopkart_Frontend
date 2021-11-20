490a452a910042d7242cbfa6134cfadc8c424653
= <=>
2021-11-20 13:51:46 +0530

    fetch products using api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,8 +8,14 @@ export class ProductService {
 
   constructor(private auth: ApiService) { }
 
-  async fetchProduct(productId: string) {
+  async fetchProduct(productId:string) {
     return await fetch(`http://localhost:3300/products/${productId}`)
+      .then((res) => res.json())
+      .then((data) => data.data)
+  }
+
+  async fetchProductsByCategory(category:string) {
+    return await fetch(`http://localhost:3300/products/category/${category}`)
       .then((res) => res.json())
       .then((data) => data.data)
   }
