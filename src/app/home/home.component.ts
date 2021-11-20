f2b9a218dd1668f56125f31567d9ec543ee99cbd
= <=>
2021-11-20 13:50:32 +0530

    get products from db and category

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productApi: ProductService) {
 
+  }
+  // @Output() product: any;
   ngOnInit(): void {
   }
+
+  displayCategories = ['electronics', 'clothing', 'food']
+  // categories:any = []
+  // async displayProducts() {
+  //   this.displayCategories.map(async (category) => {
+  //     let temp:any = {}
+  //     temp[category] = await this.getProductsByCategory(category)
+  //     this.categories.push(temp)
+  //   })
+
+  //   console.log("Display: ", this.categories)
+  // }
+
+  // async getProductsByCategory(category:string) {
+  //   category = category.toLowerCase()
+  //   let products = await this.productApi.fetchProductsByCategory(category)
+  //   return products
+  // }
 
 }
