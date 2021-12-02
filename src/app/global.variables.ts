973294de4126ad4303f24d981f79825c1c1b7c1e
= <=>
2021-12-02 22:41:30 +0530

    export variables available to all components (hostedBackend)

---
+++ src/app/global.variables.ts
@@ -0,0 +1,7 @@
+// This file contains variables available to all components in Angular
+
+"use strict";
+
+export const hostedAPI = "https://backend-shopkart.herokuapp.com"
+export const localAPI = "http://localhost:3300/"
+
